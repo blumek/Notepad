50efba9fcd755dcc1ac5d82d892d721e710cca39
Blumek <blumczynski.szymon@gmail.com>
2020-04-11 13:48:19 +0200

    Add DecodedNoteContentRepository

---
+++ app/src/main/java/com/blumek/notepad/adapter/repository/DecodedNoteContentRepository.java
@@ -0,0 +1,38 @@
+package com.blumek.notepad.adapter.repository;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Transformations;
+
+import com.blumek.notepad.domain.entity.Note;
+import com.blumek.notepad.domain.port.NoteContentDecoder;
+import com.blumek.notepad.domain.port.NoteRepository;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class DecodedNoteContentRepository extends ForwardingNoteRepository implements NoteRepository {
+    private final NoteContentDecoder noteContentDecoder;
+
+    public DecodedNoteContentRepository(NoteRepository noteRepository,
+                                        NoteContentDecoder noteContentDecoder) {
+        super(noteRepository);
+        this.noteContentDecoder = noteContentDecoder;
+    }
+
+    @Override
+    public LiveData<Note> findById(String id) {
+        return Transformations.map(super.findById(id), noteContentDecoder::decodeContent);
+    }
+
+    @Override
+    public LiveData<List<Note>> findAll() {
+        return Transformations.map(super.findAll(), this::decodeNotesContents);
+    }
+
+    private List<Note> decodeNotesContents(List<Note> notes) {
+        List<Note> decodedNotes = Lists.newArrayList();
+        for (Note note : notes)
+            decodedNotes.add(noteContentDecoder.decodeContent(note));
+        return decodedNotes;
+    }
+}
