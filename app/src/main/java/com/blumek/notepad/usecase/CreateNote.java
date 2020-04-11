02830c56a6fae716895bab6b86179015bba0eb2f
Blumek <blumczynski.szymon@gmail.com>
2020-04-11 11:13:29 +0200

    Move ID generation and encoding out of CreateNote

--- app/src/main/java/com/blumek/notepad/usecase/CreateNote.java
+++ app/src/main/java/com/blumek/notepad/usecase/CreateNote.java
@@ -2,40 +2,21 @@ package com.blumek.notepad.usecase;
 
 import com.blumek.notepad.domain.entity.Note;
 import com.blumek.notepad.domain.exception.InvalidNoteException;
-import com.blumek.notepad.domain.port.IdGenerator;
-import com.blumek.notepad.domain.port.NoteEncoder;
 import com.blumek.notepad.domain.port.NoteRepository;
 
 import static com.blumek.notepad.usecase.validator.NoteValidator.isValid;
 
 public final class CreateNote {
     private final NoteRepository noteRepository;
-    private final IdGenerator idGenerator;
-    private final NoteEncoder noteEncoder;
 
-    public CreateNote(NoteRepository noteRepository, IdGenerator idGenerator,
-                      NoteEncoder noteEncoder) {
+    public CreateNote(NoteRepository noteRepository) {
         this.noteRepository = noteRepository;
-        this.idGenerator = idGenerator;
-        this.noteEncoder = noteEncoder;
     }
 
-    public Note create(Note note) {
+    public void create(Note note) {
         if (!isValid(note))
             throw new InvalidNoteException();
 
-        Note noteToCreate = Note.builder()
-                .id(idGenerator.generate())
-                .title(note.getTitle())
-                .content(note.getContent())
-                .password(note.getPassword())
-                .build();
-
-        Note encodedNote = noteEncoder.encode(noteToCreate);
-        noteRepository.create(encodedNote);
-
-        return noteToCreate.toBuilder()
-                .password(encodedNote.getPassword())
-                .build();
+        noteRepository.create(note);
     }
 }
