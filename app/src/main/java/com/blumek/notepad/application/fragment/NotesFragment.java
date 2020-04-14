1c11c8b3783a5ee0bfc11f2fdf6dcb7a049105ba
Blumek <blumczynski.szymon@gmail.com>
2020-04-14 15:38:19 +0200

    Add RecyclerView to NotesFragment

--- app/src/main/java/com/blumek/notepad/application/fragment/NotesFragment.java
+++ app/src/main/java/com/blumek/notepad/application/fragment/NotesFragment.java
@@ -12,14 +12,23 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.blumek.notepad.R;
+import com.blumek.notepad.adapter.repository.RoomNoteRepository;
+import com.blumek.notepad.adapter.repository.dao.NoteDao;
+import com.blumek.notepad.application.adapter.NotesAdapter;
+import com.blumek.notepad.application.database.AppDatabase;
 import com.blumek.notepad.application.view_model.NotesViewModel;
+import com.blumek.notepad.application.view_model.factory.NotesViewModelFactory;
 import com.blumek.notepad.databinding.NotesFragmentBinding;
+import com.blumek.notepad.usecase.FindNote;
 
 public class NotesFragment extends Fragment {
     private NotesViewModel viewModel;
+    private NotesAdapter notesAdapter;
 
-    public static NotesFragment newInstance() {
-        return new NotesFragment();
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        notesAdapter = new NotesAdapter();
     }
 
     @Override
@@ -28,13 +37,26 @@ public class NotesFragment extends Fragment {
         NotesFragmentBinding binding = DataBindingUtil.inflate(inflater, R.layout.notes_fragment,
                 container, false);
 
+        binding.setAdapter(notesAdapter);
+
         return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        viewModel = new ViewModelProvider(this).get(NotesViewModel.class);
+        NoteDao noteDao = AppDatabase.getInstance(getActivity()).noteDao();
+        RoomNoteRepository noteRepository = new RoomNoteRepository(noteDao);
+        FindNote findNote = new FindNote(noteRepository);
+        NotesViewModelFactory notesViewModelFactory = new NotesViewModelFactory(findNote);
+        viewModel = new ViewModelProvider(this, notesViewModelFactory)
+                .get(NotesViewModel.class);
+
+        observeNotes();
+    }
+
+    private void observeNotes() {
+        viewModel.getViewNotes().observe(getViewLifecycleOwner(), notesAdapter::setViewNotes);
     }
 
 }
